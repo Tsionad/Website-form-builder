2c2c08fecb101dba1eb88c6e40ef5288e6701beb
Tsion Adefres <tadefres@gmail.com>
2018-12-14 00:43:15 -0800

    Added contact form components and configs

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
+// form-specific functions
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+//email service
+import { MessageService } from '../message.service';
+
+
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
@@ -7,9 +14,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
-  constructor() { }
+  contactForm: FormGroup;
+  submitted = false;
+  success = false;
+  fail = false;
+
+  constructor(private formBuilder: FormBuilder, private MessageService: MessageService ) { }
 
   ngOnInit() {
+    this.contactForm = this.formBuilder.group({
+      'email': [null, Validators.compose([
+        Validators.required,
+        Validators.email,
+      ])],
+      'subject': [null, Validators.required],
+      'message': [null, Validators.required]
+    });
   }
 
+  onSubmit() {
+    this.submitted = true;
+
+    if (this.contactForm.valid) {
+      this.success = true;
+      this.fail = false;
+
+      this.MessageService.sendMessage(this.contactForm)
+      .subscribe(
+        (response) =>{
+          alert("Thank you! Your message was sent");
+        },
+        (err) =>{
+          alert("Sorry, there was an error submitting your message, please try again");
+        }
+      );
+    }
+
+  }
 }
+  
+  
